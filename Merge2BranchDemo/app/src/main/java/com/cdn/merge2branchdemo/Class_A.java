54700c522466fdc492064cae9e5865b77185f434
rahulsonpaliya <rahulsonpaliya@cdnsol.com>
2017-12-27 13:51:17 +0530

    Made some changes in class A, to test git-merge

--- Merge2BranchDemo/app/src/main/java/com/cdn/merge2branchdemo/Class_A.java
+++ Merge2BranchDemo/app/src/main/java/com/cdn/merge2branchdemo/Class_A.java
@@ -11,6 +11,8 @@ public class Class_A {
 
     public static void showMsg(Context context){
         Toast.makeText(context, "Hello World from Class A", Toast.LENGTH_SHORT).show();
+        //made this change to check if git-merge proper works
+        Toast.makeText(context, "Toast after merge", Toast.LENGTH_SHORT).show();
     }
 
 }
