cdeccb701f04b2037c4e9e8e909e9fe915ae75fe
rahulsonpaliya <rahulsonpaliya@cdnsol.com>
2017-12-27 13:49:23 +0530

    Merged branches 'A' and 'B' into Merged_Branch_C

---
+++ Merge2BranchDemo/app/src/main/java/com/cdn/merge2branchdemo/Class_A.java
@@ -0,0 +1,16 @@
+package com.cdn.merge2branchdemo;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Rahul Sonpaliya on 27/12/17.
+ */
+
+public class Class_A {
+
+    public static void showMsg(Context context){
+        Toast.makeText(context, "Hello World from Class A", Toast.LENGTH_SHORT).show();
+    }
+
+}
