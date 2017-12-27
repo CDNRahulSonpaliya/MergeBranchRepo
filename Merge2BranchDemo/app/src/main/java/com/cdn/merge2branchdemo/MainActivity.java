f483d61acc187a6d28793d3d673989b1462a5c1a
rahulsonpaliya <rahulsonpaliya@cdnsol.com>
2017-12-27 14:23:15 +0530

    changing main-activity from Branch-B

--- Merge2BranchDemo/app/src/main/java/com/cdn/merge2branchdemo/MainActivity.java
+++ Merge2BranchDemo/app/src/main/java/com/cdn/merge2branchdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.cdn.merge2branchdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //changing main-activity from Branch-B
+        Toast.makeText(this, "toast in main-activity of branch B", Toast.LENGTH_SHORT).show();
     }
 }
